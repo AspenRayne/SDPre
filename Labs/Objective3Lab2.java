e139f3a56018eef60c5c7dccef852735f0cf2e24
AspenRayne <90653438+AspenRayne@users.noreply.github.com>
2022-07-27 14:19:38 -0600

    Objective 3 Labs

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2{
+    public static void main(String[] args){
+        double num1 = 10, num2 = 8; 
+        double sum = (num1 * num1) + (num2 * num2);
+        double result = Math.sqrt(sum);
+
+        System.out.println("The hypotenuse of a triangle with sides" + " "  + num1 + " " + "and" + " " + num2 + " " + "is" + " " +result);
+        
+    }
+}

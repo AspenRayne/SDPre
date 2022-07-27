42cbf494f0e005e419abf76818f29b3b6709a2fe
AspenRayne <90653438+AspenRayne@users.noreply.github.com>
2022-07-26 23:53:33 -0600

    Objective 2 Labs

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+        String name = "Aspen Newman";
+        int age = 20;
+        String state = "South Dakota";
+
+        System.out.print("Name: " + name + " ");
+        System.out.print("Age: " + age + " ");
+        System.out.print("State: " + state);
+
+    }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+    public static void main(String[] args){
+        int year= 2002;
+        String month= "May";
+        boolean today= false;
+
+        System.out.println(year);
+        System.out.println(month);
+        System.out.println(today);
+    }
+}
